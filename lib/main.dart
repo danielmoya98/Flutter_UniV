793257862ac128364af184bf9c9a10076d1e3538
Daniel Moya <115199469+danielmoya98@users.noreply.github.com>
2023-08-06 00:45:33 -0400

    LOGIN

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,14 @@ import 'package:page_transition/page_transition.dart';
 
 void main() => runApp(MyApp());
 
-
 //SPLASH SCREEN//
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Clean Code',
-        debugShowCheckedModeBanner: false ,
+        title: 'Univalle Movil',
+        debugShowCheckedModeBanner: false,
         home: AnimatedSplashScreen(
             duration: 3000,
             splash: Image.asset('assets/logo.jpg'),
@@ -29,15 +28,196 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Univalle Movil',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material '),
-        ),
+        backgroundColor: Colors.white,
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                padding: const EdgeInsets.only(top: 30, bottom: 20 ),
+                child: const Text(
+                  'Univalle',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(top: 10, bottom: 50),
+                margin: const EdgeInsets.only(left: 10 , right: 10),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        'Bienvenido',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        'LA RESPUESTA CIENTIFICA AL FUTUTRO',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      width: 350.0,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Usuario',
+                          contentPadding: EdgeInsets.all(16),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      width: 350.0,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: 'Contraseña',
+                          contentPadding: EdgeInsets.all(16),
+                          border: InputBorder.none,
+                          suffixIcon: Icon(Icons.remove_red_eye),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      width: 300.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.black, // Pintar el botón de negro
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Acción al presionar el botón
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black, // Color de fondo del botón
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: const Text(
+                          'Iniciar Sesión',
+                          style: TextStyle(
+                            color: Colors.white, // Color del texto del botón
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      'Otras Opciones',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      width: 300.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.black, // Pintar el botón de negro
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          // Acción al presionar el botón
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black, // Color de fondo del botón
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        icon: const Icon(
+                          Icons.account_box,
+                          color: Colors.white, // Color del ícono del botón
+                        ),
+                        label: const Text(
+                          'Recupera tu cuenta',
+                          style: TextStyle(
+                            color: Colors.white, // Color del texto del botón
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      width: 300.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.black, // Pintar el botón de negro
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          // Acción al presionar el botón
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black, // Color de fondo del botón
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        icon: const Icon(
+                          Icons.lock_open,
+                          color: Colors.white, // Color del ícono del botón
+                        ),
+                        label: const Text(
+                          'Desbloquea tu cuenta',
+                          style: TextStyle(
+                            color: Colors.white, // Color del texto del botón
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
